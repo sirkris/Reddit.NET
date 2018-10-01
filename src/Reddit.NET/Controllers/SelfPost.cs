f41480e5ef4c48d9bf8816dc39632201f2bca56c
Kris Craig <KrisCraig@php.net>
2018-10-01 02:34:28 -0700

    Basic retrieval method.

--- src/Reddit.NET/Controllers/SelfPost.cs
+++ src/Reddit.NET/Controllers/SelfPost.cs
@@ -10,12 +10,34 @@ namespace Reddit.NET.Controllers
         public string SelfText;
         public string SelfTextHTML;
 
+        /// <summary>
+        /// Create new Self Post instance from Reddit API listing.
+        /// </summary>
+        /// <param name="listing">Listing returned by Reddit API.</param>
         public SelfPost(Listing listing) : base(listing)
         {
             this.SelfText = listing.SelfText;
             this.SelfTextHTML = listing.SelfTextHTML;
         }
 
+        /// <summary>
+        /// Create a new Self Post instance and populate manually.
+        /// </summary>
+        /// <param name="subreddit">The subreddit the post belongs to.</param>
+        /// <param name="title">Post title.</param>
+        /// <param name="author">Reddit user who authored the post.</param>
+        /// <param name="selfText">The post body.</param>
+        /// <param name="selfTextHtml">The HTML-formateed post body.</param>
+        /// <param name="id">Post ID.</param>
+        /// <param name="name">Post name.</param>
+        /// <param name="permalink">Permalink of post.</param>
+        /// <param name="created">When the post was created.</param>
+        /// <param name="edited">When the post was last edited.</param>
+        /// <param name="score">Net vote score.</param>
+        /// <param name="upVotes">Number of upvotes.</param>
+        /// <param name="downVotes">Number of downvotes.</param>
+        /// <param name="removed">Whether the post was removed.</param>
+        /// <param name="spam">Whether the post was marked as spam.</param>
         public SelfPost(string subreddit, string title, string author, string selfText, string selfTextHtml,
             string id = null, string name = null, string permalink = null, DateTime created = default(DateTime),
             DateTime edited = default(DateTime), int score = 0, int upVotes = 0, int downVotes = 0,
@@ -29,6 +51,20 @@ namespace Reddit.NET.Controllers
             this.Listing = new Listing(this);
         }
 
+        /// <summary>
+        /// Query the Reddit API and populate this new instance with the result.
+        /// </summary>
+        /// <param name="postId">The Reddit post ID.</param>
+        public SelfPost(string postId)
+        {
+            GetByPostId(postId);
+
+            this.Listing = new Listing(this);
+        }
+
+        /// <summary>
+        /// Create an empty SelfPost instance.
+        /// </summary>
         public SelfPost() { }
 
         /// <summary>
@@ -56,6 +92,15 @@ namespace Reddit.NET.Controllers
         {
             return (!String.IsNullOrWhiteSpace(Subreddit)
                 && !String.IsNullOrWhiteSpace(Title));
+        }
+
+        /// <summary>
+        /// Query the Reddit API and populate this instance with the result.
+        /// </summary>
+        /// <param name="postId">The Reddit post ID.</param>
+        private void GetByPostId(string postId)
+        {
+            // TODO
         }
     }
 }
