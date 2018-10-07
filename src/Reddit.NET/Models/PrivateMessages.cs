7f241a9914d07d359d6ef4be0e947af948268b10
Kris Craig <KrisCraig@php.net>
2018-10-07 00:00:40 -0700

    Private Messages endpoints.

--- src/Reddit.NET/Models/PrivateMessages.cs
+++ src/Reddit.NET/Models/PrivateMessages.cs
@@ -12,5 +12,108 @@ namespace Reddit.NET.Models
         internal override RestClient RestClient { get; set; }
 
         public PrivateMessages(string appId, string refreshToken, string accessToken, RestClient restClient) : base(appId, refreshToken, accessToken, restClient) { }
+
+        public object Block(string id)
+        {
+            RestRequest restRequest = PrepareRequest("api/block", Method.POST);
+
+            restRequest.AddParameter("id", id);
+
+            return JsonConvert.DeserializeObject(ExecuteRequest(restRequest));
+        }
+
+        public object CollapseMessage(string id)
+        {
+            RestRequest restRequest = PrepareRequest("api/collapse_message", Method.POST);
+
+            restRequest.AddParameter("id", id);
+
+            return JsonConvert.DeserializeObject(ExecuteRequest(restRequest));
+        }
+
+        public object Compose(string fromSr, string gRecaptchaResponse, string subject, string text, string to)
+        {
+            RestRequest restRequest = PrepareRequest("api/compose", Method.POST);
+
+            restRequest.AddParameter("from_sr", fromSr);
+            restRequest.AddParameter("g-recaptcha-response", gRecaptchaResponse);
+            restRequest.AddParameter("subject", subject);
+            restRequest.AddParameter("text", text);
+            restRequest.AddParameter("to", to);
+
+            return JsonConvert.DeserializeObject(ExecuteRequest(restRequest));
+        }
+
+        public object DelMsg(string id)
+        {
+            RestRequest restRequest = PrepareRequest("api/del_msg", Method.POST);
+
+            restRequest.AddParameter("id", id);
+
+            return JsonConvert.DeserializeObject(ExecuteRequest(restRequest));
+        }
+
+        public object ReadAllMessages(string filterTypes)
+        {
+            RestRequest restRequest = PrepareRequest("api/read_all_messages", Method.POST);
+
+            restRequest.AddParameter("filter_types", filterTypes);
+
+            return JsonConvert.DeserializeObject(ExecuteRequest(restRequest));
+        }
+
+        public object ReadMessage(string id)
+        {
+            RestRequest restRequest = PrepareRequest("api/read_message", Method.POST);
+
+            restRequest.AddParameter("id", id);
+
+            return JsonConvert.DeserializeObject(ExecuteRequest(restRequest));
+        }
+
+        public object UnblockSubreddit(string id)
+        {
+            RestRequest restRequest = PrepareRequest("api/unblock_subreddit", Method.POST);
+
+            restRequest.AddParameter("id", id);
+
+            return JsonConvert.DeserializeObject(ExecuteRequest(restRequest));
+        }
+
+        public object UncollapseMessage(string id)
+        {
+            RestRequest restRequest = PrepareRequest("api/uncollapse_message", Method.POST);
+
+            restRequest.AddParameter("id", id);
+
+            return JsonConvert.DeserializeObject(ExecuteRequest(restRequest));
+        }
+
+        public object UnreadMessage(string id)
+        {
+            RestRequest restRequest = PrepareRequest("api/unread_message", Method.POST);
+
+            restRequest.AddParameter("id", id);
+
+            return JsonConvert.DeserializeObject(ExecuteRequest(restRequest));
+        }
+
+        public object GetMessages(string where, bool mark, string mid, string after, string before, bool includeCategories, int count = 0,
+            int limit = 25, string show = "all", bool srDetail = false)
+        {
+            RestRequest restRequest = PrepareRequest("message/" + where);
+
+            restRequest.AddParameter("mark", mark);
+            restRequest.AddParameter("mid", mid);
+            restRequest.AddParameter("after", after);
+            restRequest.AddParameter("before", before);
+            restRequest.AddParameter("include_categories", includeCategories);
+            restRequest.AddParameter("count", count);
+            restRequest.AddParameter("limit", limit);
+            restRequest.AddParameter("show", show);
+            restRequest.AddParameter("sr_detail", srDetail);
+
+            return JsonConvert.DeserializeObject(ExecuteRequest(restRequest));
+        }
     }
 }
