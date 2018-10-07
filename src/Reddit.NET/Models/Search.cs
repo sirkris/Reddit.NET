c6ce3bddfb5e738ffa11a216d4135901e8c9ead0
Kris Craig <KrisCraig@php.net>
2018-10-07 06:35:02 -0700

    Search endpoint.

--- src/Reddit.NET/Models/Search.cs
+++ src/Reddit.NET/Models/Search.cs
@@ -12,5 +12,26 @@ namespace Reddit.NET.Models
         internal override RestClient RestClient { get; set; }
 
         public Search(string appId, string refreshToken, string accessToken, RestClient restClient) : base(appId, refreshToken, accessToken, restClient) { }
+
+        public object GetSearch(string after, string before, string category, bool includeFacets, string q, bool restrictSr, string sort, string t,
+            string subreddit = null, int count = 0, int limit = 25, string show = "all", bool srDetail = false, string type = null)
+        {
+            RestRequest restRequest = PrepareRequest(Sr(subreddit) + "search");
+
+            restRequest.AddParameter("after", after);
+            restRequest.AddParameter("before", before);
+            restRequest.AddParameter("category", category);
+            restRequest.AddParameter("include_facets", includeFacets);
+            restRequest.AddParameter("restrict_sr", restrictSr);
+            restRequest.AddParameter("sort", sort);
+            restRequest.AddParameter("t", t);
+            restRequest.AddParameter("count", count);
+            restRequest.AddParameter("limit", limit);
+            restRequest.AddParameter("show", show);
+            restRequest.AddParameter("sr_detail", srDetail);
+            restRequest.AddParameter("type", type);
+
+            return JsonConvert.DeserializeObject(ExecuteRequest(restRequest));
+        }
     }
 }
