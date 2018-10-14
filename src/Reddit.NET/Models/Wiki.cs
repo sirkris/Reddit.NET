69c0d0dfdba3438c4c1ff8e5197c4861ad8b944d
Kris Craig <KrisCraig@php.net>
2018-10-14 02:00:03 -0700

    Wiki.

--- src/Reddit.NET/Models/Wiki.cs
+++ src/Reddit.NET/Models/Wiki.cs
@@ -15,11 +15,11 @@ namespace Reddit.NET.Models
 
         // TODO - Needs testing.
         /// <summary>
-        /// 
+        /// Allow username to edit this wiki page.
         /// </summary>
-        /// <param name="page"></param>
-        /// <param name="username"></param>
-        /// <param name="subreddit"></param>
+        /// <param name="page">the name of an existing wiki page</param>
+        /// <param name="username">the name of an existing user</param>
+        /// <param name="subreddit">The subreddit where the wiki lives</param>
         /// <returns>(TODO - Untested)</returns>
         public object AllowEditor(string page, string username, string subreddit = null)
         {
@@ -33,11 +33,11 @@ namespace Reddit.NET.Models
 
         // TODO - Needs testing.
         /// <summary>
-        /// 
+        /// Deny username to edit this wiki page.
         /// </summary>
-        /// <param name="page"></param>
-        /// <param name="username"></param>
-        /// <param name="subreddit"></param>
+        /// <param name="page">the name of an existing wiki page</param>
+        /// <param name="username">the name of an existing user</param>
+        /// <param name="subreddit">The subreddit where the wiki lives</param>
         /// <returns>(TODO - Untested)</returns>
         public object DenyEditor(string page, string username, string subreddit = null)
         {
@@ -51,13 +51,13 @@ namespace Reddit.NET.Models
 
         // TODO - Needs testing.
         /// <summary>
-        /// 
+        /// Edit a wiki page.
         /// </summary>
         /// <param name="content"></param>
-        /// <param name="page"></param>
-        /// <param name="previous"></param>
-        /// <param name="reason"></param>
-        /// <param name="subreddit"></param>
+        /// <param name="page">the name of an existing page or a new page to create</param>
+        /// <param name="previous">the starting point revision for this edit</param>
+        /// <param name="reason">a string up to 256 characters long, consisting of printable characters</param>
+        /// <param name="subreddit">The subreddit where the wiki lives</param>
         /// <returns>(TODO - Untested)</returns>
         public object Edit(string content, string page, string previous, string reason, string subreddit = null)
         {
@@ -72,11 +72,11 @@ namespace Reddit.NET.Models
 
         // TODO - Needs testing.
         /// <summary>
-        /// 
+        /// Toggle the public visibility of a wiki page revision.
         /// </summary>
-        /// <param name="page"></param>
-        /// <param name="revision"></param>
-        /// <param name="subreddit"></param>
+        /// <param name="page">the name of an existing wiki page</param>
+        /// <param name="revision">a wiki revision ID</param>
+        /// <param name="subreddit">The subreddit where the wiki lives</param>
         /// <returns>(TODO - Untested)</returns>
         public object Hide(string page, string revision, string subreddit = null)
         {
@@ -90,11 +90,11 @@ namespace Reddit.NET.Models
 
         // TODO - Needs testing.
         /// <summary>
-        /// 
+        /// Revert a wiki page to revision.
         /// </summary>
-        /// <param name="page"></param>
-        /// <param name="revision"></param>
-        /// <param name="subreddit"></param>
+        /// <param name="page">the name of an existing wiki page</param>
+        /// <param name="revision">a wiki revision ID</param>
+        /// <param name="subreddit">The subreddit where the wiki lives</param>
         /// <returns>(TODO - Untested)</returns>
         public object Revert(string page, string revision, string subreddit = null)
         {
@@ -108,14 +108,15 @@ namespace Reddit.NET.Models
 
         // TODO - Needs testing.
         /// <summary>
-        /// 
+        /// Retrieve a list of discussions about this wiki page.
+        /// This endpoint is a listing.
         /// </summary>
-        /// <param name="page"></param>
+        /// <param name="page">the name of an existing wiki page</param>
         /// <param name="after">fullname of a thing</param>
         /// <param name="before">fullname of a thing</param>
-        /// <param name="subreddit"></param>
-        /// <param name="count"></param>
-        /// <param name="limit"></param>
+        /// <param name="subreddit">The subreddit where the wiki lives</param>
+        /// <param name="count">a positive integer (default: 0)</param>
+        /// <param name="limit">the maximum number of items desired (default: 25, maximum: 100)</param>
         /// <param name="show">(optional) the string all</param>
         /// <param name="srDetail">(optional) expand subreddits</param>
         /// <returns>(TODO - Untested)</returns>
@@ -134,26 +135,26 @@ namespace Reddit.NET.Models
         }
 
         /// <summary>
-        /// 
+        /// Retrieve a list of wiki pages in this subreddit.
         /// </summary>
-        /// <param name="subreddit"></param>
-        /// <returns></returns>
+        /// <param name="subreddit">The subreddit where the wiki lives</param>
+        /// <returns>A list of wiki pages.</returns>
         public object Pages(string subreddit = null)
         {
             return JsonConvert.DeserializeObject(ExecuteRequest(Sr(subreddit) + "wiki/pages"));
         }
 
         /// <summary>
-        /// 
+        /// Retrieve a list of recently changed wiki pages in this subreddit.
         /// </summary>
         /// <param name="after">fullname of a thing</param>
         /// <param name="before">fullname of a thing</param>
-        /// <param name="subreddit"></param>
-        /// <param name="count"></param>
-        /// <param name="limit"></param>
+        /// <param name="subreddit">The subreddit where the wiki lives</param>
+        /// <param name="count">a positive integer (default: 0)</param>
+        /// <param name="limit">the maximum number of items desired (default: 25, maximum: 100)</param>
         /// <param name="show">(optional) the string all</param>
         /// <param name="srDetail">(optional) expand subreddits</param>
-        /// <returns></returns>
+        /// <returns>A list of wiki pages.</returns>
         public object Revisions(string after, string before, string subreddit = null, int count = 0, int limit = 25, string show = "all", bool srDetail = false)
         {
             RestRequest restRequest = PrepareRequest(Sr(subreddit) + "wiki/revisions");
@@ -169,17 +170,18 @@ namespace Reddit.NET.Models
         }
 
         /// <summary>
-        /// 
+        /// Retrieve a list of revisions of this wiki page.
+        /// This endpoint is a listing.
         /// </summary>
-        /// <param name="page"></param>
+        /// <param name="page">the name of an existing wiki page</param>
         /// <param name="after">fullname of a thing</param>
         /// <param name="before">fullname of a thing</param>
-        /// <param name="subreddit"></param>
-        /// <param name="count"></param>
-        /// <param name="limit"></param>
+        /// <param name="subreddit">The subreddit where the wiki lives</param>
+        /// <param name="count">a positive integer (default: 0)</param>
+        /// <param name="limit">the maximum number of items desired (default: 25, maximum: 100)</param>
         /// <param name="show">(optional) the string all</param>
         /// <param name="srDetail">(optional) expand subreddits</param>
-        /// <returns></returns>
+        /// <returns>A list of revisions.</returns>
         public object PageRevisions(string page, string after, string before, string subreddit = null, int count = 0, int limit = 25, string show = "all", bool srDetail = false)
         {
             RestRequest restRequest = PrepareRequest(Sr(subreddit) + "wiki/revisions/" + page);
@@ -195,11 +197,11 @@ namespace Reddit.NET.Models
         }
 
         /// <summary>
-        /// 
+        /// Retrieve the current permission settings for page.
         /// </summary>
-        /// <param name="page"></param>
-        /// <param name="subreddit"></param>
-        /// <returns></returns>
+        /// <param name="page">the name of an existing wiki page</param>
+        /// <param name="subreddit">The subreddit where the wiki lives</param>
+        /// <returns>An object containing wiki page settings.</returns>
         public object GetPermissions(string page, string subreddit = null)
         {
             return JsonConvert.DeserializeObject(ExecuteRequest(Sr(subreddit) + "wiki/settings/" + page));
@@ -207,12 +209,12 @@ namespace Reddit.NET.Models
 
         // TODO - Needs testing.
         /// <summary>
-        /// 
+        /// Update the permissions and visibility of wiki page.
         /// </summary>
-        /// <param name="page"></param>
+        /// <param name="page">the name of an existing wiki page</param>
         /// <param name="listed">boolean value</param>
-        /// <param name="permLevel"></param>
-        /// <param name="subreddit"></param>
+        /// <param name="permLevel">an integer</param>
+        /// <param name="subreddit">The subreddit where the wiki lives</param>
         /// <returns>(TODO - Untested)</returns>
         public object UpdatePermissions(string page, bool listed, int permLevel, string subreddit = null)
         {
@@ -225,13 +227,14 @@ namespace Reddit.NET.Models
         }
 
         /// <summary>
-        /// 
+        /// Return the content of a wiki page.
+        /// If v is given, show the wiki page as it was at that version If both v and v2 are given, show a diff of the two.
         /// </summary>
-        /// <param name="page"></param>
-        /// <param name="v"></param>
-        /// <param name="v2"></param>
-        /// <param name="subreddit"></param>
-        /// <returns></returns>
+        /// <param name="page">the name of an existing wiki page</param>
+        /// <param name="v">a wiki revision ID</param>
+        /// <param name="v2">a wiki revision ID</param>
+        /// <param name="subreddit">The subreddit where the wiki lives</param>
+        /// <returns>An object containing wiki page data.</returns>
         public object Page(string page, string v, string v2, string subreddit = null)
         {
             RestRequest restRequest = PrepareRequest(Sr(subreddit) + "wiki/" + page);
