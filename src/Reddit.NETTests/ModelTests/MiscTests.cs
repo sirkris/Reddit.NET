1e691da6aa99a91b7c225e1beb40c8d27e788eac
Kris Craig <KrisCraig@php.net>
2018-10-22 00:16:36 -0700

    Misc.

--- src/Reddit.NETTests/ModelTests/MiscTests.cs
+++ src/Reddit.NETTests/ModelTests/MiscTests.cs
@@ -10,6 +10,16 @@ namespace Reddit.NETTests.ModelTests
     [TestClass]
     public class MiscTests : BaseTests
     {
+        [TestMethod]
+        public void Scopes()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
 
+            Dictionary<string, Scope> scopes = reddit.Models.Misc.Scopes();
+
+            Assert.IsNotNull(scopes);
+            Assert.IsTrue(scopes.Count > 0);
+        }
     }
 }
