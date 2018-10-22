1358c610eac95fa92313185c3fe86bd34795a0d8
Kris Craig <KrisCraig@php.net>
2018-10-21 22:28:45 -0700

    Flairs.

--- src/Reddit.NETTests/ModelTests/FlairTests.cs
+++ src/Reddit.NETTests/ModelTests/FlairTests.cs
@@ -10,6 +10,27 @@ namespace Reddit.NETTests.ModelTests
     [TestClass]
     public class FlairTests : BaseTests
     {
-        
+        [TestMethod]
+        public void UserFlair()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
+
+            List<Flair> flairs = reddit.Models.Flair.UserFlair("Political_Revolution");
+
+            Assert.IsNotNull(flairs);
+            Assert.IsTrue(flairs.Count > 0);
+        }
+
+        public void UserFlairV2()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
+
+            List<FlairV2> flairs = reddit.Models.Flair.UserFlairV2("Political_Revolution");
+
+            Assert.IsNotNull(flairs);
+            Assert.IsTrue(flairs.Count > 0);
+        }
     }
 }
