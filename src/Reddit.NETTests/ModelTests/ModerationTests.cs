0540669828a7049cf730cd8005c0cdae29e1e81b
Kris Craig <KrisCraig@php.net>
2018-10-22 02:12:20 -0700

    Moderation.

--- src/Reddit.NETTests/ModelTests/ModerationTests.cs
+++ src/Reddit.NETTests/ModelTests/ModerationTests.cs
@@ -10,6 +10,70 @@ namespace Reddit.NETTests.ModelTests
     [TestClass]
     public class ModerationTests : BaseTests
     {
+        [TestMethod]
+        public void GetLog()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
 
+            ModActionContainer log = reddit.Models.Moderation.GetLog(null, null, testData["Subreddit"]);
+
+            Assert.IsNotNull(log);
+        }
+
+        [TestMethod]
+        public void ModQueueReports()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
+
+            PostContainer modQueue = reddit.Models.Moderation.ModQueue("reports", null, null, "links", testData["Subreddit"]);
+
+            Assert.IsNotNull(modQueue);
+        }
+
+        [TestMethod]
+        public void ModQueueSpam()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
+
+            PostContainer modQueue = reddit.Models.Moderation.ModQueue("spam", null, null, "comments", testData["Subreddit"]);
+
+            Assert.IsNotNull(modQueue);
+        }
+
+        [TestMethod]
+        public void ModQueue()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
+
+            PostContainer modQueue = reddit.Models.Moderation.ModQueue("modqueue", null, null, "links", testData["Subreddit"]);
+
+            Assert.IsNotNull(modQueue);
+        }
+
+        [TestMethod]
+        public void ModQueueUnmoderated()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
+
+            PostContainer modQueue = reddit.Models.Moderation.ModQueue("unmoderated", null, null, "links", testData["Subreddit"]);
+
+            Assert.IsNotNull(modQueue);
+        }
+
+        [TestMethod]
+        public void ModQueueEdited()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
+
+            PostContainer modQueue = reddit.Models.Moderation.ModQueue("edited", null, null, "links", testData["Subreddit"]);
+
+            Assert.IsNotNull(modQueue);
+        }
     }
 }
