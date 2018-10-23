b5f173e7634d1c6de020b46e0a1838216104deff
Kris Craig <KrisCraig@php.net>
2018-10-22 23:44:06 -0700

    Modmail.

--- src/Reddit.NETTests/ModelTests/ModmailTests.cs
+++ src/Reddit.NETTests/ModelTests/ModmailTests.cs
@@ -10,6 +10,26 @@ namespace Reddit.NETTests.ModelTests
     [TestClass]
     public class ModmailTests : BaseTests
     {
+        [TestMethod]
+        public void Subreddits()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
 
+            ModmailSubredditContainer subs = reddit.Models.Modmail.Subreddits();
+
+            Assert.IsNotNull(subs);
+        }
+
+        [TestMethod]
+        public void UnreadCount()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
+
+            ModmailUnreadCount unreadCount = reddit.Models.Modmail.UnreadCount();
+
+            Assert.IsNotNull(unreadCount);
+        }
     }
 }
