8afb14a491d13bba1907ad46b2aea879b4d822db
Kris Craig <KrisCraig@php.net>
2018-10-23 00:20:54 -0700

    PrivateMessages.

--- src/Reddit.NETTests/ModelTests/PrivateMessagesTests.cs
+++ src/Reddit.NETTests/ModelTests/PrivateMessagesTests.cs
@@ -10,6 +10,57 @@ namespace Reddit.NETTests.ModelTests
     [TestClass]
     public class PrivateMessagesTests : BaseTests
     {
+        [TestMethod]
+        public void GetMessagesInbox()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
 
+            MessageContainer messages = reddit.Models.PrivateMessages.GetMessages("inbox", false, "", "", "", false);
+
+
+        }
+
+        [TestMethod]
+        public void GetMessagesUnread()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
+
+            MessageContainer messages = reddit.Models.PrivateMessages.GetMessages("unread", false, "", "", "", false);
+
+
+        }
+
+        [TestMethod]
+        public void GetMessagesSent()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
+
+            MessageContainer messages = reddit.Models.PrivateMessages.GetMessages("sent", false, "", "", "", false);
+
+
+        }
+
+        [TestMethod]
+        public void Compose()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
+
+            GenericContainer res = reddit.Models.PrivateMessages.Compose("", "", "Test Message", "This is a test.  So there.", "RedditDotNetBot");
+
+
+        }
+
+        [TestMethod]
+        public void ComposeWithSr()
+        {
+            Dictionary<string, string> testData = GetData();
+            RedditAPI reddit = new RedditAPI(testData["AppId"], testData["RefreshToken"]);
+
+            GenericContainer res = reddit.Models.PrivateMessages.Compose(testData["Subreddit"], "", "Test Message", "This is a test.  So there.", "RedditDotNetBot");
+        }
     }
 }
