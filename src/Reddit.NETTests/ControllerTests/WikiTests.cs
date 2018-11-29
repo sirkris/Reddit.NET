c9a25b0a2b3536f6455e0690de4c2d2bc20b2f9e
Kris Craig <KrisCraig@php.net>
2018-11-29 01:41:05 -0800

    WikiPage tests.

--- src/Reddit.NETTests/ControllerTests/WikiTests.cs
+++ src/Reddit.NETTests/ControllerTests/WikiTests.cs
@@ -24,7 +24,33 @@ namespace Reddit.NETTests.ControllerTests
         }
         private Subreddit subreddit;
 
+        private WikiPage Index
+        {
+            get
+            {
+                return index ?? GetIndex();
+            }
+            set
+            {
+                index = value;
+            }
+        }
+        private WikiPage index;
+
         public WikiTests() : base() { }
+
+        private WikiPage GetIndex()
+        {
+            Index = Subreddit.Wiki.GetPage("index");
+            return Index;
+        }
+
+        [TestMethod]
+        public void About()
+        {
+            Validate(Index);
+            Assert.AreEqual("index", Index.Name);
+        }
 
         [TestMethod]
         public void Pages()
@@ -54,6 +80,24 @@ namespace Reddit.NETTests.ControllerTests
         public void GetRecentPageRevisions()
         {
             Validate(Subreddit.Wiki.GetRecentPageRevisions());
+        }
+
+        [TestMethod]
+        public void Revisions()
+        {
+            Validate(Index.Revisions());
+        }
+
+        [TestMethod]
+        public void GetPermissions()
+        {
+            Validate(Index.GetPermissions());
+        }
+
+        [TestMethod]
+        public void UpdatePermissions()
+        {
+            Validate(Index.UpdatePermissions(true, 0));
         }
     }
 }
