dca670993a7e1b1ae6307bed5d7ba9a9814870c4
Kris Craig <KrisCraig@php.net>
2018-12-08 23:55:24 -0800

    Subreddit tests.

---
+++ src/Reddit.NETTests/ControllerTests/WorkflowTests/SubredditTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reddit.NET;
+using Reddit.NET.Controllers;
+using Reddit.NET.Controllers.EventArgs;
+using Reddit.NET.Exceptions;
+using RedditThings = Reddit.NET.Models.Structures;
+using System;
+using System.Collections.Generic;
+
+namespace Reddit.NETTests.ControllerTests.WorkflowTests
+{
+    [TestClass]
+    public class SubredditTests : BaseTests
+    {
+        private Subreddit Subreddit
+        {
+            get
+            {
+                return subreddit ?? GetSubreddit();
+            }
+            set
+            {
+                subreddit = value;
+            }
+        }
+        private Subreddit subreddit;
+
+        public SubredditTests() : base() { }
+
+        private Subreddit GetSubreddit()
+        {
+            Subreddit = reddit.Subreddit(testData["Subreddit"]).About();
+            return Subreddit;
+        }
+
+        [TestMethod]
+        public void SubredditImages()
+        {
+            // Get the embedded test images.  --Kris
+            byte[] imageData = GetResourceFile("birdie.png");
+            byte[] imageBannerData = GetResourceFile("banner.jpg");
+            byte[] imageIconData = GetResourceFile("birdie256.jpg");
+
+            // Add the images.  --Kris
+            Validate(Subreddit.UploadHeader(imageData));
+            Validate(Subreddit.UploadImg(imageData, "birdie"));
+            Validate(Subreddit.UploadIcon(imageIconData));
+            Validate(Subreddit.UploadBanner(imageBannerData));
+
+            // Delete the images.  --Kris
+            Subreddit.DeleteHeader();
+            Subreddit.DeleteImg("birdie");
+            Subreddit.DeleteIcon();
+            Subreddit.DeleteBanner();
+        }
+    }
+}
