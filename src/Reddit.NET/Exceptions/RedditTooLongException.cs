bf25115768699bbfc95ab2ef3e80fc8ec828c97c
Kris Craig <KrisCraig@php.net>
2019-02-01 02:58:03 -0800

    Library now throws custom exceptions for 'already submitted' and 'too long' responses.

---
+++ src/Reddit.NET/Exceptions/RedditTooLongException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Reddit.Exceptions
+{
+    public class RedditTooLongException : Exception
+    {
+        public RedditTooLongException(string message, Exception inner)
+            : base(message, inner) { }
+
+        public RedditTooLongException(string message)
+            : base(message) { }
+
+        public RedditTooLongException() { }
+    }
+}
