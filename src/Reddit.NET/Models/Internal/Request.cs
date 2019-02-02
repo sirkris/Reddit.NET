15d9a574222169b96a2d256d6b0c05b29eb62fb7
Kris Craig <KrisCraig@php.net>
2019-02-02 01:15:00 -0800

    Automatic retry when API returns Service Unavailable response.

--- src/Reddit.NET/Models/Internal/Request.cs
+++ src/Reddit.NET/Models/Internal/Request.cs
@@ -155,26 +155,38 @@ namespace Reddit.Models.Internal
 
         private IRestResponse GetResponse(IRestResponse res, ref RestRequest restRequest)
         {
-            int retry = 5;
-            while ((res == null || !res.IsSuccessful)
-                    && (RefreshToken != null || DeviceId != null)
-                    && (res.StatusCode == HttpStatusCode.Unauthorized  // This is returned if the access token needs to be refreshed or wasn't provided.  --Kris
-                        || res.StatusCode == HttpStatusCode.InternalServerError  // On rare occasion, a valid request will return a status code of 500, particularly if under heavy load.  --Kris
-                        || res.StatusCode == 0)  // On rare occasion, a valid request will return a status code of 0, particularly if under heavy load.  --Kris
-                    && retry > 0)
+            int serviceRetry = 3;
+            do
             {
-                /*
-                 * If it fails and we have a refresh token, request a new access token and retry.
-                 * Note that this workflow will not work if you pass an empty access token, as the Reddit API will still return 200 on those requests.
-                 * Therefore, if you just want to get a new access token from refresh, pass an arbitrary string value instead of null.
-                 * 
-                 * --Kris
-                 */
-                restRequest = RefreshAccessToken(restRequest);
-                res = RestClient.Execute(restRequest);
+                int retry = 5;
+                while ((res == null || !res.IsSuccessful)
+                        && (RefreshToken != null || DeviceId != null)
+                        && (res.StatusCode == HttpStatusCode.Unauthorized  // This is returned if the access token needs to be refreshed or wasn't provided.  --Kris
+                            || res.StatusCode == HttpStatusCode.InternalServerError  // On rare occasion, a valid request will return a status code of 500, particularly if under heavy load.  --Kris
+                            || res.StatusCode == 0)  // On rare occasion, a valid request will return a status code of 0, particularly if under heavy load.  --Kris
+                        && retry > 0)
+                {
+                    /*
+                     * If it fails and we have a refresh token, request a new access token and retry.
+                     * Note that this workflow will not work if you pass an empty access token, as the Reddit API will still return 200 on those requests.
+                     * Therefore, if you just want to get a new access token from refresh, pass an arbitrary string value instead of null.
+                     * 
+                     * --Kris
+                     */
+                    restRequest = RefreshAccessToken(restRequest);
+                    res = RestClient.Execute(restRequest);
 
-                retry--;
-            }
+                    retry--;
+                }
+
+                // If API returns "Service Unavailable", wait a few seconds then retry.  --Kris
+                serviceRetry--;
+                if (serviceRetry > 0
+                    && res.StatusCode == HttpStatusCode.ServiceUnavailable)
+                {
+                    Thread.Sleep(3000);
+                }
+            } while ((res == null || res.StatusCode == HttpStatusCode.ServiceUnavailable) && serviceRetry > 0);
 
             return res;
         }
