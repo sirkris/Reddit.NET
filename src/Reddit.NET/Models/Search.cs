bafbc1e1f1e8ee31bc81b7e403acc5015651c913
Kris Craig <KrisCraig@php.net>
2019-03-16 00:18:53 -0700

    Additional search methods.  If you're specifying multiple types in your search, use the new MultiSearch method.

--- src/Reddit.NET/Models/Search.cs
+++ src/Reddit.NET/Models/Search.cs
@@ -1,4 +1,5 @@
-﻿using Reddit.Inputs.Search;
+﻿using Newtonsoft.Json;
+using Reddit.Inputs.Search;
 using Reddit.Things;
 using RestSharp;
 
@@ -21,6 +22,73 @@ namespace Reddit.Models
         public T GetSearch<T>(SearchGetSearchInput searchGetSearchInput, string subreddit = null)
         {
             return SendRequest<T>(Sr(subreddit) + "search", searchGetSearchInput);
+        }
+
+        /// <summary>
+        /// Search Reddit and return the results as subreddit listings.
+        /// </summary>
+        /// <param name="searchGetSearchInput">A valid SearchGetSearchInput instance</param>
+        /// <param name="subreddit">The subreddit being searched</param>
+        /// <returns>A listing of subreddits that match the search criteria.</returns>
+        public SubredditContainer SearchSubreddits(SearchGetSearchInput searchGetSearchInput, string subreddit = null)
+        {
+            searchGetSearchInput.type = "sr";
+            return GetSearch<SubredditContainer>(searchGetSearchInput, subreddit);
+        }
+
+        /// <summary>
+        /// Search Reddit and return the results as post listings.
+        /// </summary>
+        /// <param name="searchGetSearchInput">A valid SearchGetSearchInput instance</param>
+        /// <param name="subreddit">The subreddit being searched</param>
+        /// <returns>A listing of posts that match the search criteria.</returns>
+        public PostContainer SearchPosts(SearchGetSearchInput searchGetSearchInput, string subreddit = null)
+        {
+            searchGetSearchInput.type = "link";
+            return GetSearch<PostContainer>(searchGetSearchInput, subreddit);
+        }
+
+        /// <summary>
+        /// Search Reddit and return the results as user listings.
+        /// </summary>
+        /// <param name="searchGetSearchInput">A valid SearchGetSearchInput instance</param>
+        /// <param name="subreddit">The subreddit being searched</param>
+        /// <returns>A listing of users that match the search criteria.</returns>
+        public UserContainer SearchUsers(SearchGetSearchInput searchGetSearchInput, string subreddit = null)
+        {
+            searchGetSearchInput.type = "user";
+            return GetSearch<UserContainer>(searchGetSearchInput, subreddit);
+        }
+
+        /// <summary>
+        /// Search Reddit and return the results as mixed listings.
+        /// Use this method if you're specifying multiple values for the "type" parameter.
+        /// </summary>
+        /// <param name="searchGetSearchInput">A valid SearchGetSearchInput instance</param>
+        /// <param name="subreddit">The subreddit being searched</param>
+        /// <returns>A listing of things that match the search criteria.</returns>
+        public MultiSearchResults MultiSearch(SearchGetSearchInput searchGetSearchInput, string subreddit = null)
+        {
+            MixedListingContainer mix = GetSearch<MixedListingContainer>(searchGetSearchInput, subreddit);
+
+            MultiSearchResults res = new MultiSearchResults();
+            foreach (MixedListingChild mixedListingChild in mix.Data.Children)
+            {
+                switch (mixedListingChild.Kind)
+                {
+                    case "t2":
+                        res.Users.Add(JsonConvert.DeserializeObject<User>(JsonConvert.SerializeObject(mixedListingChild.Data)));
+                        break;
+                    case "t3":
+                        res.Posts.Add(JsonConvert.DeserializeObject<Post>(JsonConvert.SerializeObject(mixedListingChild.Data)));
+                        break;
+                    case "t5":
+                        res.Subreddits.Add(JsonConvert.DeserializeObject<Subreddit>(JsonConvert.SerializeObject(mixedListingChild.Data)));
+                        break;
+                }
+            }
+            
+            return res;
         }
     }
 }
---
+++ src/Reddit.NET/Things/MixedListing/MixedListingChild.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Reddit.Things
+{
+    [Serializable]
+    public class MixedListingChild : BaseContainer
+    {
+        [JsonProperty("data")]
+        public JObject Data;
+    }
+}
