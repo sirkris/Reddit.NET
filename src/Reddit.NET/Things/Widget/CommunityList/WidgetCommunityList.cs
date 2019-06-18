31b445957610412c1ecdac95d1a530689adcf48b
Kris Craig <KrisCraig@php.net>
2019-06-17 23:36:54 -0700

    Reddit added a required 'description' field to the JSON input for widget community-list kind.

--- src/Reddit.NET/Things/Widget/CommunityList/WidgetCommunityList.cs
+++ src/Reddit.NET/Things/Widget/CommunityList/WidgetCommunityList.cs
@@ -10,15 +10,19 @@ namespace Reddit.Things
         [JsonProperty("data")]
         public List<string> Data { get; set; }  // List of subreddit names.  --Kris
 
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
         [JsonProperty("shortName")]
         public string ShortName { get; set; }
 
         [JsonProperty("styles")]
         public WidgetStyles Styles { get; set; }
 
-        public WidgetCommunityList(List<string> data, string shortName, WidgetStyles styles)
+        public WidgetCommunityList(List<string> data, string description, string shortName, WidgetStyles styles)
         {
             Data = data;
+            Description = description;
             ShortName = shortName;
             Styles = styles;
             Kind = "community-list";
