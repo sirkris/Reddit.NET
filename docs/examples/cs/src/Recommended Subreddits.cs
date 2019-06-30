17853ecc92513377da972bbe478effcb816ba3b3
Kris Craig <KrisCraig@php.net>
2019-06-30 02:46:00 -0700

    Source files.

---
+++ docs/examples/cs/src/Recommended Subreddits.cs
@@ -0,0 +1,15 @@
+using Reddit;
+using Reddit.Inputs.Subreddits;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reddit = new RedditAPI("YourRedditAppID", "YourBotUserRefreshToken");
+
+			var recommended = reddit.Models.Subreddits.Recommended("MySub", new SubredditsRecommendInput());
+        }
+    }
+}
