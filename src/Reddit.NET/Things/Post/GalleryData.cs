ba17e9faba60efedc361a25254d2f0817430b4d6
eldarium <eldarium@gmail.com>
2022-06-15 20:46:41 +0200

    gallery support

---
+++ src/Reddit.NET/Things/Post/GalleryData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Reddit.NET.Things.Post
+{
+    [Serializable]
+    public class GalleryData
+    {
+        [JsonProperty("items")]
+        public GalleryDataItem[] Items { get; set; }
+    }
+}
---
+++ src/Reddit.NET/Things/Post/MediaMetadataItems.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reddit.NET.Things.Post
+{
+    [Serializable]
+    public class MediaMetadataItem
+    {
+            public string status { get; set; }
+            public string e { get; set; }
+            public string m { get; set; }
+            public List<O> o { get; set; }
+            public List<P> p { get; set; }
+            public S s { get; set; }
+            public string id { get; set; }
+    }
+
+    [Serializable]
+    public class O
+    {
+        public int y { get; set; }
+        public int x { get; set; }
+        public string u { get; set; }
+    }
+
+    [Serializable]
+    public class P
+    {
+        public int y { get; set; }
+        public int x { get; set; }
+        public string u { get; set; }
+    }
+
+    [Serializable]
+    public class S
+    {
+        public int y { get; set; }
+        public int x { get; set; }
+        public string u { get; set; }
+    }
+}
